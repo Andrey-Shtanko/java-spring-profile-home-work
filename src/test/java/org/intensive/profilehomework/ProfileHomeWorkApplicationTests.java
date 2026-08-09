c0580a27c4be717b2506cf59ce120da2c147d190
Andrii Shtanko <andrii.shtanko@pitchbook.com>
2026-08-09 20:23:05 +0300

    init

---
+++ src/test/java/org/intensive/profilehomework/ProfileHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.intensive.profilehomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
